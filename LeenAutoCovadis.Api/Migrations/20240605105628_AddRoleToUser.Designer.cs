2e65c517cca2f2099925f4371ea67cc56500820d
jesse <jesse.groenen@student.graafschapcollege.nl>
2024-06-05 13:12:56 +0200

    Added role adding migration

--- LeenAutoCovadis.Api/Migrations/20240605105628_AddRoleToUser.Designer.cs
+++ LeenAutoCovadis.Api/Migrations/20240605105628_AddRoleToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LeenAutoCovadis.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeenAutoCovadis.Api.Migrations
 {
     [DbContext(typeof(CovadisContext))]
-    partial class CovadisContextModelSnapshot : ModelSnapshot
+    [Migration("20240605105628_AddRoleToUser")]
+    partial class AddRoleToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
